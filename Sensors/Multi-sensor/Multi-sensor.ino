c793db239c55fa6b89a024d4173724a215716118
Kevin Lam <LamKokMan@outlook.com>
2025-05-19 11:13:02 +0800

    commit

--- Sensors/Multi-sensor/Multi-sensor.ino
+++ Sensors/Multi-sensor/Multi-sensor.ino
@@ -1,9 +1,9 @@
-const uint8_t sensorCount = 3; // 传感器数量
-const uint8_t pirSensorPin = 2
+const uint8_t sensorCount = 2; // 传感器数量
+const uint8_t lightSensorPin = 2;
 const uint8_t ledPin = 13; 
 uint8_t sensorPins[sensorCount]; 
 
-int pirSensorState = 0; // 初始化PIR传感器状态
+int lightSensorState = 0; // 初始化光敏传感器状态
 int sensorState[sensorCount] = {0}; // 初始化其余传感器状态
 
 int detectSensor(int sensorPin){
@@ -20,35 +20,42 @@ void setup() {
     for(uint8_t i = 0; i < sensorCount; i++) {
         sensorPins[i] = i + 3; // 自动化赋值传感器引脚
     } //自动化赋值传感器
-
     for(uint8_t i = 0; i < sensorCount; i++) {
         pinMode(sensorPins[i], INPUT); // 设置传感器引脚为输入模式
     }
-    pinMode(pirSensorPin, INPUT); // 设置PIR传感器引脚为输入模式
+    pinMode(lightSensorPin, INPUT); // 设置光敏传感器引脚为输入模式
     pinMode(ledPin, OUTPUT); // 设置LED引脚为输出模式
     Serial.begin(9600); // 初始化串口通信
 }
 
 void loop() {
     for(uint8_t i = 0; i < sensorCount; i++) {
-        pirSensorPin = sensorPins[i]; // 读取传感器引脚
         sensorState[i] = detectSensor(sensorPins[i]); // 检测传感器状态
         Serial.print("Sensor ");
         Serial.print(i);
         Serial.print(": ");
         Serial.println(sensorState[i]);
     }
-    pirSensorState = detectSensor(pirSensorPin); // 检测PIR传感器状态
-    Serial.print("PIR Sensor: ");
-    Serial.println(pirSensorState);
+    lightSensorState = detectSensor(lightSensorPin); // 检测光敏传感器状态
+    Serial.print("light Sensor: ");
+    Serial.println(lightSensorState);
 
     // 控制LED灯
-    if (pirSensorState == 0 || sensorState[1] == 1 || sensorState[2] == 1) {
-        digitalWrite(ledPin, HIGH); // 打开LED灯
-    } else {
-        digitalWrite(ledPin, LOW); // 关闭LED灯
+    bool allSensorTriggered = false;
+    for(uint8_t i = 0; i < sensorCount; i++) {
+        if(sensorState[i] == 1) {
+            allSensorTriggered = true;
+        }
     }
+
+        if (lightSensorState == 1 && allSensorTriggered) {
+            digitalWrite(ledPin, HIGH); // 打开LED灯
+        } else {
+            digitalWrite(ledPin, LOW); // 关闭LED灯
+        }
+
     delay(500);
+
 }
 
 
